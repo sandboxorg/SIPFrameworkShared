e21dc5e3c99dee8e22a4a6dca4a22178804b5dca
Mark Raymond <mark.raymond@red-gate.com>
2015-03-27 11:20:15 +0000

    Add methods to allow implementers to indicate whether a menu item is enabled and checked.

--- SIPFrameworkShared/OeMenuItemBases/ActionSimpleOeMenuItemBase.cs
+++ SIPFrameworkShared/OeMenuItemBases/ActionSimpleOeMenuItemBase.cs
@@ -1,7 +1,13 @@
 ﻿namespace RedGate.SIPFrameworkShared
 {
-    public abstract class ActionSimpleOeMenuItemBase: SimpleOeMenuItemBase
+    public abstract class ActionSimpleOeMenuItemBase : SimpleOeMenuItemBase
     {
         public abstract void OnAction(ObjectExplorerNodeDescriptorBase node);
+    }
+
+    public abstract class ActionSimpleOeMenuItemBase2 : ActionSimpleOeMenuItemBase
+    {
+        public virtual bool EnabledFor(ObjectExplorerNodeDescriptorBase oeNode) { return true; }
+        public virtual bool CheckedFor(ObjectExplorerNodeDescriptorBase oeNode) { return false; }
     }
 }
